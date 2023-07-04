489dfc72df404919f6136118f1f4949db63fae37
ConnorDelacruz14 <Connord1432@gmail.com>
2023-07-04 13:32:39 -0700

    Header login styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const picture = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
-function Header(image, username) {
+function Header() {
   const [isOpenDropDown, setOpenDropDown] = useState(false);
   const toggleDropDown = () => {
     setOpenDropDown(!isOpenDropDown);
+  };
+
+  const [isSignedIn, setSignedIn] = useState(false);
+  const [username, setUsername] = useState("Sign in");
+
+  const authenticate_user = () => {
+    setSignedIn(true);
+    //TODO: Handle Login and Password here
+    setUsername("connorjd");
+  };
+
+  const logout = () => {
+    setSignedIn(false);
+    setUsername("Sign in");
   };
 
   return (
@@ -22,14 +36,17 @@ function Header(image, username) {
       </div>
       <div className="profile-container">
         <img className="pfp" alt="profile" src={picture}></img>
-        <div className="username-container" onClick={toggleDropDown}>
-          <span className="username">connorjd</span>
+        <div
+          className="username-container"
+          onClick={isSignedIn ? toggleDropDown : authenticate_user}
+        >
+          <span className="username">{username}</span>
           {isOpenDropDown && (
             <div className="dropdown-content">
               <ul>
                 <li>Friends</li>
                 <li>Your Projects</li>
-                <li>Sign Out</li>
+                {isSignedIn ? <li onClick={logout}>Sign Out</li> : null}
               </ul>
             </div>
           )}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SideBar from "../components/SideBar";
 import Header from "../components/Header";
+import ProjectDisplay from "../components/ProjectDisplay";
 
 function Home() {
   return (
     <div>
       <SideBar></SideBar>
       <Header></Header>
+      <ProjectDisplay></ProjectDisplay>
     </div>
   );
 }
